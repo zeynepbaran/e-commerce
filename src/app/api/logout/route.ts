a3beb2c191a4b768e810a36b35ce9841510ae3b1
Zeynep Baran <zeynepbarann.info@gmail.com>
2025-06-15 17:06:11 +0300

    login ve Logout işlemi tamamlandı, JWT oluşturuldu. JWT cookie silindi, user profile tamamlamdı

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,10 +1,14 @@
 //Çıkış API endpoint (backend)
 import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
 
 export async function POST() {
-  // token cookie’sini sil
-  cookies().delete('token');
+  const response = NextResponse.json({ success: true });
 
-  return NextResponse.json({ success: true });
+  // Çıkış yaparken 'token' cookie’sini sil
+  response.cookies.set('token', '', {
+    maxAge: 0,
+    path: '/',
+  });
+
+  return response;
 }
